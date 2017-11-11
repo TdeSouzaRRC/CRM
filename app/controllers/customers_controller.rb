0a682b7dcaf5cc81ecc769074af0bd9c9907aabd
TdeSouzaRRC <tdesouza@academic.rrc.ca>
2017-11-11 20:00:41 +0000

    Added customer controller actions

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,4 +2,12 @@ class CustomersController < ApplicationController
   def index
     @customers = Customer.all
   end
+
+  def alphabetized
+    @customers = Customer.all.order_by("full_name")
+  end
+
+  def missing_email
+    @customers = Customer.where(:email_address => nil)
+  end
 end
